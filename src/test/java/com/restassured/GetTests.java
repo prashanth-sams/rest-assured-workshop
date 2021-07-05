c09e1ce33e0ff1a6695ece0c872fa22ddaa64a17
prashanth-sams <sams.prashanth@gmail.com>
2021-07-05 19:20:52 +0400

    initial commit

---
+++ src/test/java/com/restassured/GetTests.java
@@ -0,0 +1,45 @@
+package com.restassured;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GetTests {
+
+    @Test
+    @DisplayName("Get method - status validation")
+    void statusValidation() {
+        given().
+        when().
+            get("http://api.zippopotam.us/us/90210").
+        then().
+            assertThat().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Get method - response body validation")
+    void bodyValidation() {
+        given().
+        when().
+            get("http://api.zippopotam.us/us/90210").
+        then().
+            assertThat().body("country", equalTo("United States")).
+            assertThat().body("'post code'", equalTo("90210")).
+            assertThat().body("places[0].'place name'", equalTo("Beverly Hills"));
+    }
+
+    @Test
+    @DisplayName("Get method - response content-type validation")
+    void contentTypeValidation() {
+        given().
+        when().
+            get("http://api.zippopotam.us/us/90210").
+        then().
+            assertThat().contentType(ContentType.JSON).
+            assertThat().contentType("application/json");
+    }
+
+}
---
+++ src/test/java/com/restassured/Logs.java
@@ -0,0 +1,42 @@
+package com.restassured;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Logs {
+
+    @Test
+    @DisplayName("Log all - request")
+    void logRequest() {
+        given().
+            log().all().
+        when().
+            get("http://api.zippopotam.us/us/90210").
+        then().
+            assertThat().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Log all - response")
+    void logResponse() {
+        given().
+        when().
+            get("http://api.zippopotam.us/us/90210").
+        then().
+        log().all();
+    }
+
+    @Test
+    @DisplayName("Log body")
+    void logBody() {
+        given().
+        when().
+            get("http://api.zippopotam.us/us/90210").
+        then().
+        log().body();
+    }
+}
